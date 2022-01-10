1d6e2f829f51be47fe401b02418dda1a03bc64ba
anubhabdg7 <anubhab.dg2002@gmail.com>
2022-01-10 17:38:34 +0530

    smh

---
+++ gym_jsbsim/test_gym.py
@@ -0,0 +1,13 @@
+import gym
+import gym_jsbsim 
+
+env = gym.make("GymJsbsim-HeadingControlTask-v0")
+#env = gym.make("k-v0")
+env.reset()
+done = False
+
+while True :
+   action = env.action_space.sample()
+   state, reward, done, _ = env.step(action)
+   print(state)
+   #print(reward)
